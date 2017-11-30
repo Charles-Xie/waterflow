c9b00df41f5e92117223021eccd90d9423484d13
Chi <15900635013@163.com>
2017-11-30 11:05:04 +0800

    edit to get ip automatically.

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var bodyParser = require('body-parser');
 // var multer = require('multer');
 var fs = require('fs');
 
+// module to get ip address
+var ip = require('ip');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(multer({
@@ -17,30 +20,41 @@ app.use(express.static('public'));
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+var tip = "欢迎";
+
+var filter = require('./utils/filter.js');
+
 app.get('/', function(req, res) {
-  res.render('main', {title: 'Main', header: 'Please write something here'});
+  res.render('main', { title: '流言蜚语', header: '流言蜚语', tip: tip });
 });
 
 app.post('/send', function(req, res) {
   console.log(req.body.message);
-  // res.json(req.body);
-  fs.appendFile('message.txt', req.body.message + '\n', function(err) {
-    if(err) {
-      throw err;
-      console.log('The message %s was appended', req.body.message);
-    }
-  });
-  // res.render('main', {title: 'Main', header: 'Please write something here'});
+  // check the message received
+  if(filter.test(req.body.message)) {
+    // sensitive
+    tip = 'Big brother is watching you!';
+  }
+  else {
+    // not sensitive
+    fs.appendFile('message.txt', req.body.message + '\n', function (err) {
+      if (err) {
+        throw err;
+        console.log('The message %s was appended', req.body.message);
+      }
+    });
+    tip = '发送成功!';
+  }
   res.redirect('/');
 });
 
 port = 8080;
-// ip = "192.168.1.108";
-ip = "192.168.1.100";
-app.listen(port, ip, function(err){
+
+ip_addr = ip.address();
+app.listen(port, ip_addr, function(err){
   if(err) {
     console.error(err);
   } else {
-    console.info("Server started successfully, listening at http://%s:%s", ip, port);
+    console.info("Server started successfully, listening at http://%s:%s", ip_addr, port);
   }
 });
